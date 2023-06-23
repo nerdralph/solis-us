6d432e4bd7d4c6b83a29e5b0186db2e476f295f4
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2023-06-23 09:39:53 -0300

    initial alpha

---
+++ pzem-pvo.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# 2023 Ralph Doncaster
+# reads from Peacefair PZEM-014 and PZEM-016 power meters
+# http://en.peacefair.cn/products/601.html
+# post data to PVOutput API pvoutput.org/help/api_specification.html
+# requires pymodbus 3.x
+# one optional argument for serial port
+
+from pymodbus.client import ModbusSerialClient
+from pymodbus.register_read_message import *
+import serial.tools.list_ports as lp
+from datetime import datetime
+import math, sys, time
+
+SADDR = 1                               # default slave address
+DBG = 0                                 # debug
+
+if len(sys.argv) == 1:
+    # use first serial port found
+    dev = lp.comports()[0].device
+else:
+    dev = sys.argv[1]
+
+if DBG: print("using port", dev)
+pzem = ModbusSerialClient(port=dev, baudrate=9600) 
+pzem.connect()
+
+# read 10 regs starting at address 0
+rsp = pzem.read_input_registers(0, count=10, slave=SADDR)
+#print(rsp.registers)
+volt = round(rsp.registers[0]/10, 1)
+amp = round(rsp.registers[1]*0.001, 3)
+power = round(rsp.registers[3]/10 + rsp.registers[4]*6553.6)
+energy = rsp.registers[5]
+now = datetime.now()
+date = now.strftime("%Y%m%d") 
+hm = now.strftime("%H%M")
+data={
+    "d":date,
+    "t":hm,
+    "v1":energy,
+    "v6":volt,
+    "v5":amp,
+    }
+print(data)
