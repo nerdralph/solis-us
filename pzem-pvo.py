8b17b4dc52aa7c6efc033ff5d2bcec7f801c4470
nerdralph <ralphdoncaster@gmail.com>
2024-09-17 21:41:19 -0300

    pylint cleanup

--- pzem-pvo.py
+++ pzem-pvo.py
@@ -6,11 +6,11 @@
 # requires pymodbus 3.x
 # one optional argument for serial port
 
-from pymodbus.client import ModbusSerialClient
-from pymodbus.register_read_message import *
-import serial.tools.list_ports as lp
 from datetime import datetime
-import math, sys, time
+import sys
+
+from pymodbus.client import ModbusSerialClient
+import serial.tools.list_ports as lp
 
 SADDR = 1                               # default slave address
 DBG = 0                                 # debug
@@ -21,8 +21,10 @@ if len(sys.argv) == 1:
 else:
     dev = sys.argv[1]
 
-if DBG: print("using port", dev)
-pzem = ModbusSerialClient(port=dev, baudrate=9600) 
+if DBG:
+    print("using port", dev)
+
+pzem = ModbusSerialClient(port=dev, baudrate=9600)
 pzem.connect()
 
 # read 10 regs starting at address 0
@@ -31,9 +33,9 @@ rsp = pzem.read_input_registers(0, count=10, slave=SADDR)
 volt = rsp.registers[0]/10
 amp = rsp.registers[1]/1000
 power = round(rsp.registers[3]/10 + rsp.registers[4]*6553.6)
-energy = rsp.registers[5]
+energy = rsp.registers[5] + rsp.registers[6]*65536
 now = datetime.now()
-date = now.strftime("%Y%m%d") 
+date = now.strftime("%Y%m%d")
 hm = now.strftime("%H%M")
 data={
     "d":date,
